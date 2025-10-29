1ed39b020eb4b4affc25e6269ad1c596b2e4655c
MogensenJesse <71662157+MogensenJesse@users.noreply.github.com>
2025-10-29 10:36:56 +0100

    Update cookieNotifier.js

--- cookieNotifier.js
+++ cookieNotifier.js
@@ -1,26 +1,35 @@
-(function() {
-  // URLs of the default and golden cookie favicons
-  const defaultFavicon = 'https://orteil.dashnet.org/cookieclicker/img/favicon.ico'; // Adjust if your default favicon URL differs
-  const goldenCookieFavicon = 'https://static.wikia.nocookie.net/cookieclicker/images/5/50/GoldCookie.png';
+Game.registerMod('FaviconGoldenCookie', {
+  init: function() {
+    const defaultFavicon = 'https://orteil.dashnet.org/cookieclicker/img/favicon.ico';
+    const goldenCookieFavicon = 'https://static.wikia.nocookie.net/cookieclicker/images/5/50/GoldCookie.png';
 
-  // Function to set the favicon dynamically
-  function setFavicon(url) {
+    function setFavicon(url) {
+      let link = document.querySelector("link[rel~='icon']");
+      if (!link) {
+        link = document.createElement('link');
+        link.rel = 'icon';
+        document.getElementsByTagName('head')[0].appendChild(link);
+      }
+      link.href = url;
+    }
+
+    this.faviconInterval = setInterval(() => {
+      const goldenCookiePresent = Game.shimmers.some(shimmer => shimmer.type === 'golden');
+      if (goldenCookiePresent) {
+        setFavicon(goldenCookieFavicon);
+      } else {
+        setFavicon(defaultFavicon);
+      }
+    }, 1000);
+  },
+
+  reset: function() {
+    clearInterval(this.faviconInterval);
+    // Reset favicon on mod reset
+    const defaultFavicon = '/favicon.ico';
     let link = document.querySelector("link[rel~='icon']");
-    if (!link) {
-      link = document.createElement('link');
-      link.rel = 'icon';
-      document.getElementsByTagName('head')[0].appendChild(link);
+    if (link) {
+      link.href = defaultFavicon;
     }
-    link.href = url;
   }
-
-  // Check for golden cookies in the game's shimmer array every second
-  setInterval(function() {
-    const goldenCookiePresent = Game.shimmers.some(shimmer => shimmer.type === 'golden');
-    if (goldenCookiePresent) {
-      setFavicon(goldenCookieFavicon);
-    } else {
-      setFavicon(defaultFavicon);
-    }
-  }, 1000); // Check interval in milliseconds
-})();
+});
