a085949c6506d6b3954bf9570857d383e362f6b1
MogensenJesse <71662157+MogensenJesse@users.noreply.github.com>
2025-11-05 11:21:59 +0100

    Update cookieNotifier.js

--- cookieNotifier.js
+++ cookieNotifier.js
@@ -51,12 +51,7 @@ CookieNotifier.launch = function(){
 		}
 	};
 
-	if(typeof CCSE != 'undefined' && CCSE.ConfirmGameVersion){
-		if(CCSE.ConfirmGameVersion(CookieNotifier.name, CookieNotifier.version, CookieNotifier.GameVersion)) Game.registerMod(CookieNotifier.name, CookieNotifier);
-	}
-	else{
-		Game.registerMod(CookieNotifier.name, CookieNotifier);
-	}
+	if(CCSE.ConfirmGameVersion(CookieNotifier.name, CookieNotifier.version, CookieNotifier.GameVersion)) Game.registerMod(CookieNotifier.name, CookieNotifier);
 }
 
 
@@ -65,38 +60,8 @@ if(!CookieNotifier.isLoaded){
 		CookieNotifier.launch();
 	}
 	else{
-		// Wait for CCSE to load if it's available
-		if(typeof CCSE == 'undefined') var CCSE = {};
+		if(!CCSE) var CCSE = {};
 		if(!CCSE.postLoadHooks) CCSE.postLoadHooks = [];
 		CCSE.postLoadHooks.push(CookieNotifier.launch);
-		
-		// Fallback: register immediately if Game is ready and CCSE doesn't load
-		// This ensures the mod works even without CCSE or if CCSE loads too late
-		if(typeof Game != 'undefined' && Game.ready){
-			// Game is ready, register immediately
-			setTimeout(function(){
-				if(!CookieNotifier.isLoaded){
-					CookieNotifier.launch();
-				}
-			}, 100);
-		}
-		else if(typeof Game != 'undefined'){
-			// Wait for Game to be ready
-			Game.registerHook('onload', function(){
-				setTimeout(function(){
-					if(!CookieNotifier.isLoaded){
-						CookieNotifier.launch();
-					}
-				}, 100);
-			});
-		}
-		else{
-			// Game object doesn't exist yet, wait a bit longer
-			setTimeout(function(){
-				if(!CookieNotifier.isLoaded && typeof Game != 'undefined'){
-					CookieNotifier.launch();
-				}
-			}, 1000);
-		}
 	}
 }
